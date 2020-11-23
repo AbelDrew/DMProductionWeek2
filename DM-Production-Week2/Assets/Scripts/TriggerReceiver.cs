5f7f7e4bcc43b57dbb7f6fb44de9c8df5bea9618
AbelDrew <43228983+AbelDrew@users.noreply.github.com>
2020-11-23 10:09:46 +0000

    Project

---
+++ DM-Production-Week2/Assets/Scripts/TriggerReceiver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerReceiver : MonoBehaviour
+{
+  
+    public void Received()
+    {
+        Debug.Log("Received Event");
+    }
+    public void Received(GameObject receivedObject)
+    {
+        Debug.Log("Received " + receivedObject.name);
+    }
+
+
+}
